4dd3a90565a32ab86137cdbce7cc3d27aabb5e41
Igor Bobrukiewicz <igor.bobrukiewicz@gmail.com>
2024-05-07 11:16:50 +0200

    Initial commit

---
+++ src/main/java/org/uhhigor/siitask/SiiTaskApplication.java
@@ -0,0 +1,13 @@
+package org.uhhigor.siitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiiTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/uhhigor/siitask/SiiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uhhigor.siitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
